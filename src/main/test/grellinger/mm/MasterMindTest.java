7797192d1f6dbcfcba81fee33c941b5bb55e6d07
Grellinger <=>
2016-10-02 10:58:28 -0500

    initial commit of first test

---
+++ src/main/test/grellinger/mm/MasterMindTest.java
@@ -0,0 +1,14 @@
+package grellinger.mm;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MasterMindTest {
+
+	@Test
+	public void thisTestShouldAlwaysPass() {
+		assertTrue(true);
+	}
+	
+}
